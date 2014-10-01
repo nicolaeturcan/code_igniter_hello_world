4b2bf490742de9ed055a40b17c62bd4d480d7594
nicolaeturcan <nicolaeturcan@iesebre.com>
2014-10-01 21:58:28 +0200

    v5

---
+++ application/config/code_igniter_hello_world.php
@@ -0,0 +1,16 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+/*
+|--------------------------------------------------------------------------
+| Periode academic
+|--------------------------------------------------------------------------
+|
+| bla bla bla
+|
+*/
+$config['rewrite_short_tags'] = "2014-15";
+
+
+/* End of file config.php */
+/* Location: ./application/config/config.php */
