0972da3288600a201272c9adcd4f80b2aa86ff92
nicolae <nicolaeturcan@iesebre.com>
2014-10-13 13:58:20 +0200

    V7

--- application/controllers/simpatic.php
+++ application/controllers/simpatic.php
@@ -2,39 +2,34 @@
 
 class Simpatic extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
+	function __construct(){
+		parent::__construct();
+		$this->config->load('code_igniter_hello_world');
+	}
+
 	public function index()
 	{
 		//$this->load->view('welcome_message');
 		$this->load->view('hola_mon');
 	}
 	
-	/*public function pepito(){
-		$this->load->view('hola_mon');
+	public function greetings_2(){
+		$data = array();
+		
+		$name = $this->input->get_post('name');
+		$data['name']= $name;
+		$this->load->view('grettings',$data);
 	}
 
 	public function grettings($name)
 		$data= array();
 		
-		$data['parameter1'] = $name;
-		$data['parameter2'] = $name;
-		$data['parameter3'] = $name;
+		$parameter_1 = $this->input->get('parametre1');
 
-		$this->load->view('hola_mon',$data);*/
+		$data['parameter1'] = $name;
+	    $data['parametre2'] = $this->config->item('academic_period');
+	         
+	    $this->load->view('grettings',$data);
 }
 
 /* End of file welcome.php */
